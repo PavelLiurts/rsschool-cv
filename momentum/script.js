36d66f729bbd5c4f7616bebf9931f31c1f4cca61
unknown <pavel.build@gmail.com>
2021-10-23 19:34:56 +0300

    feat: add date

--- momentum/script.js
+++
@@ -1,9 +0,0 @@
-function showTime() {
-    const time = document.querySelector('.time');
-    const date = new Date();
-    const currentTime = date.toLocaleTimeString();
-    setTimeout(showTime, 1000);
-    return time.innerHTML=currentTime;
-}
-
-showTime ();
